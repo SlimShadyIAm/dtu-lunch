176103f83f76ac496ee5e05133f2cfdab470c404
SlimShadyIAm <aamir@farooq.xyz>
2023-09-28 14:05:38 +0200

    Finalize implementation

--- main.py
+++ main.py
@@ -10,10 +10,26 @@ import asyncio
 load_dotenv()  
 
 # Set up your OpenAI API key
-openai.api_key = os.getenv('OPENAI_TOKEN')
+OPENAI_TOKEN = os.getenv('OPENAI_TOKEN')
+DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
 
-def get_menu_from_chatgpt(menu, place):
-    user_prompt = f"Today's date is {datetime.datetime.now().strftime('%A, the %d/%m/%Y')} in week {datetime.datetime.now().isocalendar().week}. The menu for today at place {place} is: ```{menu}```. What is there for lunch today? Give me just the menu, be as precise as possible, don't use adjectives. Include the name of the place in the title. Make the menu options not all capitalized, and translate to English."
+if OPENAI_TOKEN is None:
+    raise Exception("You need to set OPENAI_TOKEN environment variable to run this script")
+
+if DISCORD_WEBHOOK_URL is None:
+    raise Exception("You need to set DISCORD_WEBHOOK_URL environment variable to run this script")
+
+openai.api_key = OPENAI_TOKEN
+
+def get_menu_from_chatgpt(menu):
+    menu = menu.lower()
+    menu = menu.replace("mandag", "monday")
+    menu = menu.replace("tirsdag", "tuesday")
+    menu = menu.replace("onsdag", "wednesday")
+    menu = menu.replace("torsdag", "thursday")
+    menu = menu.replace("fredag", "friday")
+
+    user_prompt = f"Today is {datetime.datetime.now().strftime('%A')} in week {datetime.datetime.now().isocalendar().week}. The menu is: ```{menu}```. What is there for lunch today ({datetime.datetime.now().strftime('%A')}? Give me just the menu, be as precise as possible, don't use adjectives, I only want the menu options as bullet points and I don't want the name of the place or any extra information. Make the menu options not all capitalized, and translate to English."
     messages= [{"role": "user", "content": user_prompt}]
     response = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
@@ -25,37 +41,51 @@ def get_menu_from_chatgpt(menu, place):
 
 # Scrape the websites
 urls = ["https://www.foodandco.dk/besog-os-her/restauranter/dtu/dtu-lyngby-101/", "https://www.foodandco.dk/besog-os-her/restauranter/dtu/dtu-lyngby-202/", "https://www.foodandco.dk/besog-os-her/restauranter/dtu/dtu-lyngby-skylab/"]
+responses = []
 for url in urls:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
 # Extract the information you want to post
-    title = soup.find('title').text
+        place_name = soup.find('title').text
 
-    mydivs = soup.find_all("div", {"class": "ContentBlock"})
-    if len(mydivs) < 1:
-        raise Exception()
-    menu = mydivs[0].text
+        mydivs = soup.find_all("div", {"class": "ContentBlock"})
+        if len(mydivs) < 1:
+            raise Exception()
+        menu = mydivs[0].text
 
-    print("======")
+        print("======")
 # Make a request to ChatGPT
-    chat_reply = get_menu_from_chatgpt(menu, title)
+        chat_reply = get_menu_from_chatgpt(menu)
 
-    print("ChatGPT response:", chat_reply)
+        responses.append({
+                    "title": place_name,
+                    "description": chat_reply,
+                    "color": 0x00ff00
+                })
+        print(f"Successfully processed the menu for {url}")
+    except:
+        print(f"An error occured processing the menu for {url}")
 
-    # Set up the Discord webhook
-    webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
+print("======")
+# Set up the Discord webhook
+webhook_url = DISCORD_WEBHOOK_URL
 
-    # Create the payload to send in POST request
-    payload = {
-        "content": f"Title: {title}\nDescription: {chat_reply}",
-    }
+# Create the payload to send in POST request
+payload = {
+    "embeds": responses
+}
 
-    # Make the POST request to the webhook using aiohttp
-    async def post_to_webhook():
-        async with aiohttp.ClientSession() as session:
-            async with session.post(webhook_url, json=payload) as response:
-                if response.status != 204:
-                    print("Failed to post to webhook")
+DISCORD_ROLE_TO_PING = os.getenv('DISCORD_ROLE_TO_PING')
+if DISCORD_ROLE_TO_PING is not None:
+    payload["content"] = f"<@&{DISCORD_ROLE_TO_PING}>"
 
-    asyncio.run(post_to_webhook())
+# Make the POST request to the webhook using aiohttp
+async def post_to_webhook():
+    async with aiohttp.ClientSession() as session:
+        async with session.post(webhook_url, json=payload) as response:
+            if response.status != 204:
+                print("Failed to post to webhook")
+
+asyncio.run(post_to_webhook())
